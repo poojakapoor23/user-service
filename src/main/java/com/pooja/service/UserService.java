d9a0d1670d0268363016aead949da5c06ff1a470
Pooja <er.pooja.arora@gmail.com>
2025-07-23 22:19:47 +0530

    Repository Integration with MySql

--- src/main/java/com/pooja/service/UserService.java
+++ src/main/java/com/pooja/service/UserService.java
@@ -1,4 +1,14 @@
 package com.pooja.service;
 
+import com.pooja.model.User;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface UserService {
+    User saveUser(User user);
+    Optional<User> getUserById(String id);
+    List<User> getAllUsers();
+    User updateUser(String id, User user);
+    void deleteUser(String id);
 }
